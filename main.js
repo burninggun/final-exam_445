17ea5066fab384211f32dc2a1b107195c1be49f4
William Cheng <willchengsters@gmail.com>
2020-05-12 19:44:30 -0700

    mobile responsiveness and UI

---
+++ main.js
@@ -0,0 +1,9 @@
+window.attachEventHandlers('load', attachEventHandlers)
+
+function attachEventHandlers(){
+    let input1 = document.getElementById('num1');
+    let input2 = document.getElementById('num2');    
+}
+
+
+
